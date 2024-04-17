acd0b57cfb91a3482aa01139d66c01d01f318a25
mozta <perezaguirrerafael@gmail.com>
2024-04-17 14:32:48 -0600

    production configuration

--- app/backend/settings.py
+++ app/backend/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = bool(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOST').split(" ")
+# ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]']
 
 
 # Application definition
